67e9b6fb1e806a821089a1446083f1a7f1d7aa56
mtntasci <mtntasci@gmail.com>
2026-08-11 12:01:24 +0300

    chore: remove obsolete application source files and build artifacts

--- apps/mobile/next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  transpilePackages: ["@pag/shared", "lucide-react"],
-  webpack: (config) => {
-    return config;
-  }
-};
-
-module.exports = nextConfig;
